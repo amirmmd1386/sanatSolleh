a4a8899af1650ccced1d4fb41da2267f552e3c92
amirmmd1386 <hajamir1386.0@gmail.com>
2024-09-19 23:42:34 +0330

    a

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,11 @@
-var map = L.map('map').setView([35.6892, 51.3890], 13); // مختصات تهران
+document.addEventListener("DOMContentLoaded", function() {
+    var map1 = L.map('map').setView([35.6892, 51.3890], 13); // مختصات تهران
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map1);
 
-// افزودن لایه نقشه
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    var map2 = L.map('maps').setView([35.6892, 51.3890], 13); // مختصات تهران
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map2);
+});
